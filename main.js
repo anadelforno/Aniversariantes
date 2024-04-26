6e112f6b16719b75731e82d769be4cc5272eb0d8
Ana Del Forno <104856383+anadelforno@users.noreply.github.com>
2024-04-26 10:08:35 -0300

    K

--- main.js
+++ main.js
@@ -16,10 +16,10 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-06-15T23:59:00");
-const tempoObjetivo2 = new Date("2024-08-05T00:00:00");
-const tempoObjetivo3 = new Date("2024-12-30T00:00:00");
-const tempoObjetivo4 = new Date("2025-02-01T00:00:00");
+const tempoObjetivo1 = new Date("2025-04-19T23:59:00");
+const tempoObjetivo2 = new Date("2025-02-17T00:00:00");
+const tempoObjetivo3 = new Date("2025-02-21T00:00:00");
+const tempoObjetivo4 = new Date("2025-03-11T00:00:00");
 
 const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
